396de3d48f48d36104d9ff2f92acacfc9eb689d4
EdvanFontes <edvan.fontes@escolar.ifrn.edu.br>
2026-09-13 00:57:39 -0300

    mudei pra poppins a fonte e criei as rotas

--- TCC-Aguiar/src/app/app.routes.ts
+++ TCC-Aguiar/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
+import { Login } from './login/login';
+import { Cadastro } from './cadastro/cadastro';
 
 export const routes: Routes = [
   {
     path: '',
     component: Inicio
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'cadastro',
+    component: Cadastro
   }
 ];
